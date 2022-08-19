16d042ad711e8c127b66d6aff8165f7c9adb36ac
Maikon Jakson <mjakson@ciandt.com>
2022-08-19 19:33:32 -0300

    Padrões de projeto Java na prática - Parte 1 _ TASK 0 e 1

---
+++ 0x02/impressao/ImpressaoFactory.java
@@ -0,0 +1,5 @@
+public class ImpressaoFactory {
+    public static Impressao createImpressao(TamanhoImpressao tamanhoImpressao, int totalPaginas, int coloridas, boolean ehFrenteVerso){
+        return new Impressao(tamanhoImpressao, totalPaginas, coloridas, ehFrenteVerso);
+    }
+}
